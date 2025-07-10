8c84b480dc510bc84fbbbf8ff4209c77878ae125
Maximunultra <laurencepalacio099@gmail.com>
2025-07-10 19:25:34 +0800

    new update

--- Ecommerce/src/Login.jsx
+++ Ecommerce/src/Login.jsx
@@ -13,10 +13,11 @@ export default function Login({ onAuthChange }) {
     setLoading(true);
 
     try {
-      const res = await axios.post('http://localhost:5000/api/auth/login', {
-        email,
-        password,
-      });
+        const res = await axios.post(`${import.meta.env.VITE_API_URL}/api/auth/login`, {
+    email,
+    password,
+  });
+
 
       const { token, user } = res.data;
       if (token) {
